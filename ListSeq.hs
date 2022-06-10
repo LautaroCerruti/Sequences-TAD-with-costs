e3b6204ffd8a7a935b3650b7eab26dfab88a7ff2
LautaroCerruti <lautarocerruti7@gmail.com>
2022-06-10 12:04:58 -0300

    Finished implementation of ListSeq

--- ListSeq.hs
+++ ListSeq.hs
@@ -5,13 +5,18 @@ import Seq
 
 contraerSL f [] = []
 contraerSL f l@[x] = l
-contraerSL f (x:y:xs) = let (z:zs) =  f x y ||| contraerSL f xs
+contraerSL f (x:y:xs) = let (z, zs) =  f x y ||| contraerSL f xs
                          in z:zs
+
+expandirSL _ [] _ = []
+expandirSL _ [_] ys = ys
+expandirSL f (x:_:xs) (y:ys) = let (z, zs) = (f y x) ||| expandirSL f xs ys
+                                in y:z:zs
 
 instance Seq [] where
     emptyS = []
 
-    singleton a = [a]
+    singletonS a = [a]
 
     lengthS = length
 
@@ -32,8 +37,8 @@ instance Seq [] where
         in if y then x:ys else ys
 
     appendS [] ys = ys
-    append xs [] = xs
-    append (x:xs) ys = x:(append xs ys)
+    appendS xs [] = xs
+    appendS (x:xs) ys = x:(appendS xs ys)
 
     takeS l n = take n l
 
@@ -51,5 +56,12 @@ instance Seq [] where
     joinS = concat
 
     reduceS f a [] = a
-    reduceS f a [x] = f x a 
+    reduceS f a [x] = f a x 
     reduceS f a xs = reduceS f a (contraerSL f xs)
+
+    scanS f a [] = ([], a)
+    scanS f a [x] = (singletonS a, f a x)
+    scanS f a xs = let (is, r) = scanS f a (contraerSL f xs)
+                   in (expandirSL f xs is, r)
+
+    fromList = id
